4d491d3a35a0fc62b901c27f02a384a90dee0a47
Nadia Bacchus <nadiabacchus@Nadias-MacBook-Pro.local>
2022-11-17 17:26:03 -0500

    function for saving books

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from 'react';
+import { useMutation } from "@apollo/react-hooks";
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
+import { SAVE_BOOK } from "../utils/mutations";
 
 const SearchBooks = () => {
   // create state for holding returned google api data
@@ -65,10 +67,12 @@ const SearchBooks = () => {
     }
 
     try {
-      const response = await saveBook(bookToSave, token);
+      const { data } = await saveBook({
+        variables: { input: bookToSave },
+      });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
+      if (error) {
+        throw new Error("something went wrong!");
       }
 
       // if book successfully saves to user's account, save book id to state
